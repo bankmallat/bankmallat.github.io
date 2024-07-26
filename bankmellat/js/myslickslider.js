93003a6130f9d2a4eac9f5663fb6e6e474d23908
Hamed Hadi <hadispahbod@gmail.com>
2024-07-26 21:28:36 +0330

    Update project

---
+++ bankmellat/js/myslickslider.js
@@ -0,0 +1,99 @@
+﻿
+$(document).ready(function () {
+
+});
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    Speed: 3000,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    rtl: true,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                dots: false,
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                dots: false,
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                dots: false,
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                dots: true,
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 487,
+            settings: {
+
+                dots: true,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+
+        {
+            breakpoint: 355,
+            settings: {
+                dots: true,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+
+            }
+        },
+        {
+            breakpoint: 270,
+            settings: {
+                dots: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+
+            }
+        }
+
+
+    ]
+});
---
+++ bankmellat/js/script.js
@@ -0,0 +1,73 @@
+﻿jQuery(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.pmd-tabs').pmdTab();
+
+    $('.matchHeight,.matchHeight-1').matchHeight();
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        $('.matchHeight-1').matchHeight();
+
+    })
+
+    //custom function to add and remove active class
+    $(function () {
+        $(".collapse.in").parents(".panel").addClass("active");
+        $('a[data-toggle="collapse"]').on('click', function () {
+            var objectID = $(this).attr('href');
+            var expandale = $(this).attr('data-expandable');
+            if (expandale == 'true') {
+                if ($(objectID).hasClass('in')) {
+                    $(objectID).collapse('hide');
+                }
+                else {
+                    $(objectID).collapse('show');
+                }
+            }
+            $accoID = $(this).parents(".panel-group").attr("id");
+            $availiblity = $(this).parents(".panel").children(".in").length;
+            $expandable = $(this).attr("data-expandable");
+            $expanded = $(this).attr("aria-expanded");
+            $current = $(this).parent().parent().parent().parent().attr("id");
+            if ($expandable == "false") {
+                if ($expanded == "true") {
+                    $("#" + $current + " .active").removeClass("active");
+                }
+                else {
+                    $("#" + $current + " .active").removeClass("active");
+                    $(this).parents('.panel').addClass("active");
+                }
+            }
+            if ($expandable == "true") {
+                if ($expanded == "true") {
+                    $(this).parents('.panel').addClass("active");
+                }
+                else {
+                    $(this).parents('.panel').removeClass("active");
+                }
+            }
+        });
+
+    });
+ 
+    /*$('.slide').textSlider({
+
+        timeout: 15500,
+        slideTime: 500,
+        loop: 1
+
+    });*/
+
+    $('.sub-menu').hide(); //Hide children by default
+
+    $('.main-menu').children().click(function () {
+        //event.preventDefault();
+        $(this).children('.sub-menu').slideToggle('slow');
+    });
+
+    $('.drop-icon').on('click', function () {
+        event.preventDefault();
+        $(this).children('.sub-menu').slideToggle('slow');
+    });
+    
+});
